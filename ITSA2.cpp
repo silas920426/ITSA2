61e5b5c012fe951ef76bb21ac718e1a153f42d5a
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-12 22:15:38 +0800

    加入專案檔案。

---
+++ ITSA2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int MI;
+    while (cin >> MI) {
+        double KM = MI * 1.6;
+        cout << fixed << setprecision(1) << KM << "\n";
+    }
+    return 0;
+
+}
